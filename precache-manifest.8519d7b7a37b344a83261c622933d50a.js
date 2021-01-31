26e4b4b943d0a52a193ebec0e366ab5fa6ec88dd
陳eN <7947357+jhontitor1996@user.noreply.gitee.com>
2021-01-31 20:58:22 +0800

    deploy

---
+++ precache-manifest.8519d7b7a37b344a83261c622933d50a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fee3d2493c461ee97f7aa4f6c589915",
+    "url": "/my-account-1-website/QRcode.png"
+  },
+  {
+    "revision": "5d51f1802f90fb10ce15",
+    "url": "/my-account-1-website/css/app.3897ffe1.css"
+  },
+  {
+    "revision": "c99369e28d2a172a74eb96ce4b3c6990",
+    "url": "/my-account-1-website/index.html"
+  },
+  {
+    "revision": "5d51f1802f90fb10ce15",
+    "url": "/my-account-1-website/js/app.f53994e6.js"
+  },
+  {
+    "revision": "ef915b4e694cf4665e00",
+    "url": "/my-account-1-website/js/chunk-vendors.9c60dc64.js"
+  },
+  {
+    "revision": "cd8bb137d1b812708262d00c9b7db6eb",
+    "url": "/my-account-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-account-1-website/robots.txt"
+  }
+]);
